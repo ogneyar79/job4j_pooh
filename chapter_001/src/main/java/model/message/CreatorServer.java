e3d0690e894efa25e12eeb847d3b1b41caf7ba31
ogneyar <maksimus.s@mail.ru>
2020-06-25 17:23:27 +0300

    CreatorServerAdd

---
+++ chapter_001/src/main/java/model/message/CreatorServer.java
@@ -0,0 +1,54 @@
+package model.message;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class CreatorServer implements Closeable{
+    private final Socket socket;
+    private final BufferedReader reader;
+    private final BufferedWriter writer;
+
+    public CreatorServer(ServerSocket server) {
+        try {
+            this.socket = server.accept();
+            this.reader = createReader();
+            this.writer = createWriter();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void writeLine(String message) {
+        try {
+            writer.write(message);
+            writer.newLine();
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String readLine() {
+        try {
+            return reader.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        writer.close();
+        reader.close();
+        socket.close();
+    }
+
+    private BufferedWriter createWriter() throws IOException {
+        return new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+    }
+
+    private BufferedReader createReader() throws IOException {
+        return new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
+}
