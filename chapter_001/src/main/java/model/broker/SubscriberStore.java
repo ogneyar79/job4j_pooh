deb4d2c6631db9fcc9a8a97840f71ec219596226
ogneyar79 <maksimus.s@mail.ru>
2020-07-02 12:22:06 +0300

    SubscriberStoreChange

--- chapter_001/src/main/java/model/broker/SubscriberStore.java
+++ chapter_001/src/main/java/model/broker/SubscriberStore.java
@@ -12,6 +12,8 @@ public class SubscriberStore {
     // подписчикId - топик
     private final ConcurrentHashMap<String, String> subscriber;
 
+
+    // подписчик - очерьдь писем
     private final ConcurrentHashMap<String, Queue<MessageB>> mailBoxes;
 
     public SubscriberStore(ConcurrentHashMap<String, String> subscriber, ConcurrentHashMap<String, Queue<MessageB>> mailBoxes) {
@@ -19,13 +21,13 @@ public class SubscriberStore {
         this.mailBoxes = mailBoxes;
     }
 
-    public ArrayList<Queue<MessageB>> getSubsscribersBoxes(String topic){
-        ArrayList<Queue<MessageB>> listBoxes = new ArrayList<>();
-        return null;
+    public Queue<MessageB> getQueueMailBox(String idSub) {
+        return mailBoxes.get(idSub);
     }
 
+
     private void createMailBox(String id) {
-        this.mailBoxes.put(id, new ConcurrentLinkedQueue<>());
+        this.mailBoxes.put(id, new ConcurrentLinkedQueue<MessageB>());
     }
 
     public void addSubscriber(String id, String topic) {
@@ -36,7 +38,7 @@ public class SubscriberStore {
     }
 
 
-    public void deleteSubscriber(String id){
+    public void deleteSubscriber(String id) {
         subscriber.remove(id);
         mailBoxes.remove(id);
     }
@@ -46,7 +48,7 @@ public class SubscriberStore {
     }
 
     // can Null.
-    public MessageB getMessage(String id){
+    public MessageB getMessage(String id) {
         return mailBoxes.get(id).poll();
     }
 }
