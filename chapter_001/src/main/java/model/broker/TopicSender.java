dcf7897a5f405c04bf6d40dc2f02faf124032c6f
ogneyar79 <maksimus.s@mail.ru>
2020-07-02 12:21:04 +0300

    CreateMethodSearchMessageAndAddToMailBox

--- chapter_001/src/main/java/model/broker/TopicSender.java
+++ chapter_001/src/main/java/model/broker/TopicSender.java
@@ -3,35 +3,55 @@ package model.broker;
 import model.connection.Message;
 import model.message.MessageB;
 
+import java.util.Iterator;
 import java.util.Queue;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class TopicSender implements IBrokerSender {
-
 
     private final BrokerMessage brokerMessage;
 
     // String topic and queue different message
-    private final ConcurrentHashMap<String, Queue<MessageB>> topic;
+    private final ConcurrentHashMap<String, ConcurrentLinkedQueue<MessageB>> topic;
     private final SubscriberStore subscriberStore;
+    private final ConcurrentHashMap<String, ConcurrentLinkedQueue<String>> topicSubscribe;
 
 
     public TopicSender(BrokerMessage brokerMessage, SubscriberStore subscriberStore) {
         this.brokerMessage = brokerMessage;
         this.subscriberStore = subscriberStore;
         this.topic = this.brokerMessage.getTopicMap();
+        topicSubscribe = brokerMessage.getTopicSubscriber();
 
     }
 
     public void setSubscriber(String id, String subject) {
         subscriberStore.addSubscriber(id, subject);
+        topicSubscribe.get(subject).add(id);
     }
 
     public boolean searchNewMessage() {
-         if (brokerMessage.isChangerTopic())
-         {
-
-         }
+        if (brokerMessage.isChangerTopic()) {
+            Set<String> keys = topic.keySet();      //     list of topic
+            Iterator<String> keysIterator = keys.iterator();
+            while (keysIterator.hasNext()) {          //         bypass    all topics
+                String topicKey = keysIterator.next();
+                if (!topic.get(topicKey).isEmpty()) {                    // if have massage
+                    Queue<MessageB> messageBQueue = topic.get(topicKey);             // get queue message for our topic
+                    ConcurrentLinkedQueue<String> subscreber = topicSubscribe.get(topic);     // get all subscriber which subscribe for topic
+                    while (!messageBQueue.isEmpty()) {
+                        MessageB message = messageBQueue.poll();
+                        for (String sub : subscreber) {
+                            subscriberStore.getQueueMailBox(sub).add(message);              // add every subscriber message to his mailBox
+                        }
+                    }
+                }
+            }
+            brokerMessage.setChangerTopic(false);
+            return true;
+        }
         return false;
     }
 
