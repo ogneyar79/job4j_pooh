2930b18d479957673de5179560877c05b2738c2b
ogneyar79 <maksimus.s@mail.ru>
2020-07-18 10:21:18 +0300

    ServerWithClientAndThreadStart

---
+++ chapter_001/src/main/java/model/serversender/ServerSenderThread.java
@@ -0,0 +1,13 @@
+package model.serversender;
+
+public class ServerSenderThread extends Thread {
+
+    private final ServerWithClient senderClient;
+
+    public ServerSenderThread(ServerWithClient senderClient, int port) {
+        this.senderClient = senderClient;
+        this.senderClient.startServer(port);
+        this.start();
+    }
+
+}
